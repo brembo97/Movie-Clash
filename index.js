a940aa44b5a9334c231e33057423081922462079
Emilio <emilio.brenes.p@hotmail.com>
2020-10-17 10:53:58 -0600

    Logic to call API on a single movie ID and render its HTML

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const root = document.querySelector(".autoComplete");
 root.innerHTML = `
-    <label><b>Search For a Movie<b><label>
+    <label><b>Search For a Movie</b></label>
     <input class="input"/>
     <div class="dropdown">
         <div class="dropdown-menu">
@@ -13,10 +13,17 @@ root.innerHTML = `
 const input = document.querySelector(".input");
 const dropdown = document.querySelector(".dropdown");
 const results = document.querySelector(".dropdown-content");
+const summary = document.querySelector("#summary");
 
 const getInput = async event => {
     //Get API data
     const movies = await fetchData(event.target.value);
+
+    //Handle empty searches
+    if(!movies.length){
+        dropdown.classList.remove('is-active');
+        return;
+    }
 
     //Show dropdown HTML
     dropdown.classList.add("is-active");
@@ -35,8 +42,35 @@ const getInput = async event => {
         <img src="${imgSrc}">
         ${movie.Title}
         `
+        //Add event handler to option
+        option.addEventListener('click', () => {
+            //Update input
+            input.value = movie.Title;
+            dropdown.classList.remove('is-active');
+
+            //Get and render movie details
+            fetchMovieDetails(movie.imdbID);
+        })
+
         results.appendChild(option);
     }
 }
 
+const fetchMovieDetails = async movieID => {
+    const movieDetails = await axios.get("http://www.omdbapi.com/", {
+        params: {
+            apikey: "ef5afcd6",
+            i: movieID
+        }
+    })
+    
+    summary.innerHTML = movieTemplate(movieDetails.data);
+}
+
 input.addEventListener('input', debounce(getInput, 500));
+
+document.addEventListener('click', event => {
+    if(!root.contains(event.target)){
+        dropdown.classList.remove('is-active');
+    }
+})
